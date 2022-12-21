174b40712eb008471eddffcb1b79812c8f3bb64d
Julian <julianjpit@gmail.com>
2022-12-21 13:51:18 -0500

    init commit

---
+++ Turbo Cow/Database/Connection.cpp
@@ -0,0 +1,8 @@
+//
+//  Connection.cpp
+//  Turbo Cow
+//
+//  Created by Julian Pitterson on 2022-12-21.
+//
+
+#include "Connection.hpp"
---
+++ Turbo Cow/Database/Connection.hpp
@@ -0,0 +1,13 @@
+//
+//  Connection.hpp
+//  Turbo Cow
+//
+//  Created by Julian Pitterson on 2022-12-21.
+//
+
+#ifndef Connection_hpp
+#define Connection_hpp
+
+#include <stdio.h>
+//#include <cassandra.h>
+#endif /* Connection_hpp */
