fb9a96f642c9f625dffb265d17c5846fbd535e53
Julian <julianjpit@gmail.com>
2022-12-21 13:48:44 -0500

    Initial Commit

---
+++ Turbo Cow/Turbo_CowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Turbo_CowApp.swift
+//  Turbo Cow
+//
+//  Created by Julian Pitterson on 2022-12-21.
+//
+
+import SwiftUI
+
+@main
+struct Turbo_CowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
